b03aa94a96cba1465befe2befbca91c056ef74e2
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2024-07-04 14:00:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,80 @@ provider "azuread" {
 
 resource "azurerm_resource_group" "appgrp" {
   name     = "app-group-new"
-  location = "West Europe"
+  location = "East US"
+}
+
+
+
+
+
+
+
+
+resource "azurerm_virtual_network" "example" {
+  name                = "vpc-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "Azure-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_virtual_machine" "example" {
+  name                  = "test-machine"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "example-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "examplevm"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
 
 
@@ -74,4 +147,17 @@ resource "azurerm_resource_group" "appgrp" {
 // module "linuxvm" {
 //     source  =   "./azure/linuxVM"
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
