da34521a06d722f1a734dcce7ca85e463acbe3d3
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2024-07-04 14:06:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,36 +4,7 @@
 
 # Terraform Block
 
-terraform {
-    required_providers  {
-        azurerm =   {
-            source  =   "hashicorp/azurerm"
-        }
-    }
-}
 
-# Provider Block
-
-provider "azurerm" {
-    version         =   "~> 2.0"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-    
-    features {}
-}
-
-
-
-
-provider "azuread" {
-    version         =   ">= 0.11"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    tenant_id       =   var.tenant_id
-    alias           =   "ad"
-}
 
 terraform {
   required_providers {
